2aca9b03b36a985003b42972f7b52cc4a9fc0892
Dana Kimmel <dear.prudence34@gmail.com>
2021-03-09 13:41:21 -0500

    EvenOrOdd

---
+++ EvenOrOdd/src/EvenOrOddApp.java
@@ -0,0 +1,12 @@
+
+public class EvenOrOddApp {
+
+	public static void main(String[] args) {
+		
+	}
+		 public static String even_or_odd(int number) {
+			 if(number%2==0) return "Even"; 
+			 else return "Odd";   
+
+}
+}
