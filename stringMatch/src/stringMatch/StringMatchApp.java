dc999728989328b94c900d12bdcd33d3036b8d1e
Dana Kimmel <dear.prudence34@gmail.com>
2021-03-12 20:31:54 -0500

    init commit

---
+++ stringMatch/src/stringMatch/StringMatchApp.java
@@ -0,0 +1,25 @@
+package stringMatch;
+
+public class StringMatchApp {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public int stringMatch(String a, String b) {
+		int counter = 0;
+		
+		for(int i = 0; ((i < a.length() - 2) && (i < b.length() - 2 )); i++) {
+			
+			String aSubString = a.substring(i, i + 2);
+			String bSubString = b.substring(i, i + 2);
+			
+			if(aSubString.equals(bSubString)) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+
+}
